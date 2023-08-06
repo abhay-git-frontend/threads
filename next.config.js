60e51a67eff84674cbc8abe1927d66d85a7b08b3
mystrycoding2009 <91905331+mystrycoding2009@users.noreply.github.com>
2023-08-06 20:21:42 +0530

    gg

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
+    swcMinify: false,
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
